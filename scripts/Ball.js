1c2e1e88cd900caa4a836be9ba9293ba7f56b16c
Cate <caterinailariopaz@alumnos.tuniversformacion.es>
2023-11-22 09:12:32 +0100

    Error: bucle for en la función collsionDetected() para detecta la colisión entre ellas y así cambiar el color

---
+++ scripts/Ball.js
@@ -0,0 +1,61 @@
+
+
+export class Ball {
+    constructor(x, y, velX, velY, color, size) { //Error2: falta atributo color en el constructor de la clase
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.color = color;
+        this.size = size;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI); //Error3: Math.PI no es una función, es objeto nativo de js
+        ctx.fill();
+    }
+
+    update() {
+        // Error: habría que realizar 4 condiciones
+        // En la primera si hay colisión derecha: cambiamos el signo del valor absoluto de la velocidad
+        // para que se produzca un choque y un cambio de sentido
+        if ((this.x + this.size) >= width) {
+            this.velX = -(Math.abs(this.velX));
+        } 
+        
+        if ((this.x - this.size) <= 0) {
+            // Si hay colisión izquierda (x es 0 en el borde izquierdo), cambiamos el signo del valor absoluto
+            this.velX = Math.abs(this.velX);
+        }
+        // Lo mismo del eje x para el eje y
+        if ((this.y + this.size) >= height) {
+            this.velY = -(Math.abs(this.velY));
+        }
+
+        if ((this.y - this.size) <= 0) {
+            this.velY = Math.abs(this.velY);
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+
+    collisionDetect() {
+        //Error 4: Habría que crear un bucle for para recorrer el array bolas y detectar las colisiones entre ellas
+        for(const ball of balls){
+            //Comprobar que la bola no es la misma que con la que estamos iterando
+            if(!this === ball){
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + ball.size) {
+            ball.color = this.color = randomRGB();
+            }
+            }
+        }
+        
+    }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width = window.innerHeight;
+const width = canvas.width = window.innerWidth; // Error1: InnerWidth para que ocupe todo el ancho de la ventana
 const height = canvas.height = window.innerHeight;
 
 function random(min, max) {
@@ -12,66 +12,7 @@ function randomRGB() {
     return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-    constructor(x, y, velX, velY, color, size) { //Error1: falta atributo color en el constructor de la clase
-        this.x = x;
-        this.y = y;
-        this.velX = velX;
-        this.velY = velY;
-        this.color = color;
-        this.size = size;
-    }
 
-    draw() {
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI); //Error2: Math.PI no es una función, es objeto nativo de js
-        ctx.fill();
-    }
-
-    update() {
-        // Error: habría que realizar 4 condiciones
-        // En la primera si hay colisión derecha: cambiamos el signo del valor absoluto de la velocidad
-        // para que se produzca un choque y un cambio de sentido
-        if ((this.x + this.size) >= width) {
-            this.velX = -(Math.abs(this.velX));
-        } 
-        
-        if ((this.x - this.size) <= 0) {
-            // Si hay colisión izquierda (x es 0 en el borde izquierdo), cambiamos el signo del valor absoluto
-            this.velX = Math.abs(this.velX);
-        }
-        // Lo mismo del eje x para el eje y
-        if ((this.y + this.size) >= height) {
-            this.velY = -(Math.abs(this.velY));
-        }
-
-        if ((this.y - this.size) <= 0) {
-            this.velY = Math.abs(this.velY);
-        }
-
-        this.x += this.velX;
-        this.y += this.velY;
-    }
-
-
-    collisionDetect() {
-        //Error 2: Creamos un bucle for para recorrer el array bolas
-        for(const ball of balls){
-            //Comprobar que la bola no es la misma que con la que estamos iterando
-            if(!this === ball){
-            const dx = this.x - ball.x;
-            const dy = this.y - ball.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-
-            if (distance < this.size + ball.size) {
-            ball.color = this.color = randomRGB();
-            }
-            }
-        }
-        
-    }
-}
 
 
 const balls = [];
