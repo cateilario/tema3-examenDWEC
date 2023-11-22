ddb521850aa7a297052009ab7a252afb4e12ed2b
Cate <caterinailariopaz@alumnos.tuniversformacion.es>
2023-11-22 09:13:55 +0100

    module canvas-set-up

---
+++ scripts/canvasSetUp.js
@@ -0,0 +1,32 @@
+const canvas = document.querySelector('canvas');
+export const ctx = canvas.getContext('2d');
+
+export const width = canvas.width = window.innerWidth; // Error1: InnerWidth para que ocupe todo el ancho de la ventana
+export const height = canvas.height = window.innerHeight;
+
+export function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function randomRGB() {
+    return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+export function loop() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)'; //Error3: color de fondo negro transparente
+    ctx.fillRect(0, 0, width, height);
+
+    for (const ball of balls) {
+        ball.draw();
+        ball.update(); 
+        ball.collisionDetect();
+        for (const otherBall of balls) {
+            if (ball !== otherBall) {
+                ball.collisionDetect(otherBall);
+            }
+        }
+    }
+
+    requestAnimationFrame(loop);
+}
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,3 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-
-const width = canvas.width = window.innerWidth; // Error1: InnerWidth para que ocupe todo el ancho de la ventana
-const height = canvas.height = window.innerHeight;
-
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function randomRGB() {
-    return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
-}
-
 
 
 
@@ -31,22 +17,6 @@ while (balls.length < 4) {
     balls.push(ball);
 }
 
-function loop() {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)'; //Error3: color de fondo negro transparente
-    ctx.fillRect(0, 0, width, height);
 
-    for (const ball of balls) {
-        ball.draw();
-        ball.update(); 
-        ball.collisionDetect();
-        for (const otherBall of balls) {
-            if (ball !== otherBall) {
-                ball.collisionDetect(otherBall);
-            }
-        }
-    }
-
-    requestAnimationFrame(loop);
-}
 
 loop();
